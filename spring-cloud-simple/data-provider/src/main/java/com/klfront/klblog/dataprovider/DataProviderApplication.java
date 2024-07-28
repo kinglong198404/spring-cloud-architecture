abe6b5b4d2b87315a425e595b521bbe235e0e60a
lujl <lujl@gxhunter.com.cn>
2024-07-28 14:29:48 +0800

    完善微服务框架，删除data-provider2工程

--- spring-cloud-simple/data-provider/src/main/java/com/klfront/klblog/dataprovider/DataProviderApplication.java
+++ spring-cloud-simple/data-provider/src/main/java/com/klfront/klblog/dataprovider/DataProviderApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+
 @SpringBootApplication
+@EnableDiscoveryClient
+//@EnableEurekaClient只适用于Eureka作为注册中心，@EnableDiscoveryClient 可以是其他注册中心。
 public class DataProviderApplication {
 
     public static void main(String[] args) {
